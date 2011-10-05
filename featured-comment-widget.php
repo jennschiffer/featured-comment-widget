e928354ebb8f586fdd2364cbe8c4bf44bdf1e5b8
Jenn Schiffer <jenn@pancaketheorem.com>
2011-10-05 08:29:35 -0400

    initial project commit, version 1.3

---
+++ featured-comment-widget.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Plugin Name: Featured Comment Widget
+Plugin URI: http://pancaketheorem.com/featured-comment-widget
+Description: A widget that allows you to showcase any comment that has been published on your site. All you need to do is enter the comment's ID in the widget form.
+Version: 1.2
+Author: Jenn Schiffer
+Author URI: http://jennschiffer.com
+
+Copyright 2011  Jenn Schiffer  (http://jennschiffer.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+function featuredCommentWidget_init() {
+	register_widget('featuredCommentWidget');
+}
+
+add_action('init', 'featuredCommentWidget_init', 1);
+
+function featuredCommentCSS() {
+	echo '<style type="text/css">
+			.featuredComment-comment { margin: 3px auto 5px;}
+			.featuredComment-cite { display: block; text-align: left; line-height: 1em;}
+			.featuredComment-cite:after { content: "."; display: block; height: 0; clear: both; visibility: hidden; }
+				.featuredComment-gravatar {float: right; padding: 0px 5px;}
+				.featuredComment-author {float: right;}
+		  </style>';
+}
+
+add_action('wp_head', 'featuredCommentCSS');
+
+class featuredCommentWidget extends WP_Widget {
+
+	function featuredCommentWidget() {
+		$widget_ops = array('classname' => 'featured_comment_widget', 'description' => __('Enter a comment&apos;s ID to feature it on your sidebar.'));
+		$this->WP_Widget('featuredComment', __('Featured Comment'), $widget_ops);
+	}
+
+	function widget( $args, $instance ) {
+		extract($args);
+		$title = apply_filters('widget_title', $instance['title']);
+		$commentID = apply_filters( 'widget_commentID', empty($instance['commentID']) ? '2' : $instance['commentID'], $instance, $this->id_base);
+		$gravatarSize = apply_filters( 'widget_gravatarSize', empty($instance['gravatarSize']) ? '25' : $instance['gravatarSize'], $instance, $this->id_base);
+		$excerptSize = apply_filters( 'widget_excerptSize', empty($instance['excerptSize']) ? '' : $instance['excerptSize'], $instance, $this->id_base);
+		
+		echo $before_widget;
+		
+		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
+		else { echo $before_title . '' . $after_title; } 
+ 
+				$featuredComment = get_comment($commentID);
+				$featuredCommentName = $featuredComment->comment_author;
+				$featuredCommentEmail = $featuredComment->comment_author_email;
+				$featuredCommentContent = $featuredComment->comment_content;
+				$featuredCommentPostID = $featuredComment->comment_post_ID;
+				$featuredCommentURL = '/?p='.$featuredCommentPostID.'/#comment-'.$commentID;
+				
+				if ( $excerptSize != '' ) { 
+				    if (mb_strlen($featuredCommentContent) > $length) { 
+				    	$featuredCommentContent = mb_substr($featuredCommentContent,0,$excerptSize).' [<a href="'.$featuredCommentURL.'">...</a>]';
+				    }	
+				}
+			
+				echo '<div class="featuredComment-comment">'. $featuredCommentContent . '</div>';
+			?>
+			
+				<div class="featuredComment-cite">
+					<span class="featuredComment-author"><a href="<?php echo $featuredCommentURL; ?>">Posted by <br /><?php echo $featuredCommentName; ?></a></span>
+					<span class="featuredComment-gravatar"><a href="<?php echo $featuredCommentURL; ?>"><?php echo get_avatar($featuredCommentEmail, $gravatarSize); ?></a></span>
+				</div>
+			
+	<?php
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['commentID'] = strip_tags($new_instance['commentID']);
+		$instance['gravatarSize'] = strip_tags($new_instance['gravatarSize']);
+		$instance['excerptSize'] = strip_tags($new_instance['excerptSize']);
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'commentID' => '', 'gravatarSize' => '', 'title' => '') );
+		$title = strip_tags($instance['title']);
+		$commentID = strip_tags($instance['commentID']);
+		$gravatarSize = strip_tags($instance['gravatarSize']);
+		$excerptSize = strip_tags($instance['excerptSize']);
+	?>
+		<p><lable for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Widget Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+		<p><label for="<?php echo $this->get_field_id('commentID'); ?>"><?php _e('Comment ID:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('commentID'); ?>" name="<?php echo $this->get_field_name('commentID'); ?>" type="text" value="<?php echo esc_attr($commentID); ?>" /></p>
+		<p><label for="<?php echo $this->get_field_id('gravatarSize'); ?>"><?php _e('Gravatar width in pixels<br />(<em>leaving blank defaults to 25</em>):'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('gravatarSize'); ?>" name="<?php echo $this->get_field_name('gravatarSize'); ?>" type="text" value="<?php echo esc_attr($gravatarSize); ?>" /></p>
+		<p><label for="<?php echo $this->get_field_id('excerptSize'); ?>"><?php _e('Comment excerpt size in characters<br />(<em>leave blank to NOT excerpt comment content</em>):'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('excerptSize'); ?>" name="<?php echo $this->get_field_name('excerptSize'); ?>" type="text" value="<?php echo esc_attr($excerptSize); ?>" /></p>
+		
+	<?php
+	}
+	
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Plugin Name ===
+Contributors: jennschiffer
+Plugin Name: Featured Comment Widget
+Plugin URI: http://pancaketheorem.com/featured-comment-widget/
+Author: Jenn Schiffer
+Author URI: http://pancaketheorem.com/
+Donate link: http://pancaketheorem.com/contribute/
+Tags: comments, featured, sidebar
+Requires at least: 2.3
+Tested up to: 3.1
+Stable tag: 1.2
+
+A widget that allows you to showcase any comment that has been published on your site.
+
+== Description ==
+
+The Featured Comment Widget gives you the ability to shine a spotlight on some of your favorite comments on the site.
+
+Installing this plugin adds the Featured Comment Widget to your list of available widgets in the 'Widgets' submenu (which you can find under 'Appearance' in your WordPress dashboard). Every comment has an numeric ID, and putting that ID in the widget form results in the that comment, along with the name and Gravatar of its commenter, showing in the sidebar.  The Gravatar and username is automatically a link to that comment in its respective post.
+
+== Changelog ==
+
+= 1.2 =
+* Add ability to excerpt comments by a given number of characters.
+
+= 1.1 =
+* Add ability to enter custom widget title.
+
+== Installation ==
+
+1. Upload `featured-comment-widget.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the widget through the 'Widgets' submenu under 'Appearance' in WordPress
+4. Add the comment ID of the comment you want featured in the widget and save.
+
+== Screenshots ==
+
+1. Finding the comment ID to enter into your widget form.
+2. Finding the comment ID in the Dashboard.
+3. The Featured Comment Widget form.
+4. This is what the widget looks like if you're using the Twenty Ten theme by WordPress.
+5. This is what the widget looks like on my own custom themed blog, with the comments excerpted at approximately 95 characters.
